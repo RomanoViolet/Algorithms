d5b4c980628bc4f4ab9940a5d68373a8b6608a0c
RomanoViolet <19875252+RomanoViolet@users.noreply.github.com>
2020-04-15 21:39:43 +0200

    Reorganization of structure

--- Deque/BuildScripts/Run-Spotbugs-All.sh
+++ Deque/BuildScripts/Run-Spotbugs-All.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Recompile all sources into .class files
-for f in $(ls /workspaces/Algorithms/Percolation/bin/*.class)
+for f in $(ls $(pwd)/bin/*.class)
 do
     #echo "Running spotbugs on $f"
     spotbugs $f
